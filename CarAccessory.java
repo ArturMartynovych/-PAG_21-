f352a593a73fdfa5c06e5150a2ae605c2fe4c1bc
ArturMartynovych <59308583+ArturMartynovych@users.noreply.github.com>
2021-04-18 23:26:58 +0300

    Add files via upload

---
+++ CarAccessory.java
@@ -0,0 +1,29 @@
+package zad1;
+
+public class CarAccessory{
+
+    private double carAccessoryWeight;
+    private double carAccessoryPrice;
+
+    public CarAccessory(double price, double weight) {
+        this.carAccessoryPrice = price;
+        this.carAccessoryWeight = weight;
+    }
+
+    public double getCarAccessoryWeight() {
+        return carAccessoryWeight;
+    }
+
+    public void setAccessoryWeight(double carAccessoryWeight) {
+        this.carAccessoryWeight = carAccessoryWeight;
+    }
+
+    public double getCarAccessoryPrice() {
+        return carAccessoryPrice;
+    }
+
+    public void setCarAccessoryPrice(double carAccessoryPrice) {
+        this.carAccessoryPrice = carAccessoryPrice;
+    }
+
+}
